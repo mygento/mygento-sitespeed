8b70361881d31f5bb1a1c360109c23e2ba363e50
Danila Tarasov <grohotun@gmail.com>
2015-11-04 22:37:50 +0300

    new

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-node ./node_modules/sitespeed.io/bin/sitespeed.js -d 0 --sites mygento.txt --sites dd.txt -b firefox -n 1 --name "Mygento Continuous Tests" --screenshot --outputFolderName out --gpsiKey=$PSI_TOKEN
+node ./node_modules/sitespeed.io/bin/sitespeed.js -d 0 -b firefox -n 1 --name "Mygento Continuous Tests" --screenshot --outputFolderName out --gpsiKey=$PSI_TOKEN --sites mygento.txt --sites dd.txt --sites happia.txt --sites elc.txt --sites vh.txt
