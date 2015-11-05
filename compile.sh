6b59dc3c0980701503b03260955bca097f5609fe
Nikita Tarasov <nikita@mygento.ru>
2015-11-05 11:22:40 +0300

    revert to firefox

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-node ./node_modules/sitespeed.io/bin/sitespeed.js -d 0 -b chrome -n 1 --name "Mygento Continuous Tests" --screenshot --outputFolderName out --gpsiKey=$PSI_TOKEN --sites mygento.txt --sites dd.txt --sites happia.txt --sites elc.txt --sites vh.txt
+node ./node_modules/sitespeed.io/bin/sitespeed.js -d 0 -b firefox -n 1 --name "Mygento Continuous Tests" --screenshot --outputFolderName out --gpsiKey=$PSI_TOKEN --sites mygento.txt --sites dd.txt --sites happia.txt --sites elc.txt --sites vh.txt
